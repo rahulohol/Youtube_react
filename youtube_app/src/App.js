5e98afa887781e9dcd5f2cfd2fa044f6023f938f
Rahul Ohol <nevergiupyou123@gmail.com>
2023-01-10 20:18:31 +0530

    nav

--- youtube_app/src/App.js
+++ youtube_app/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Box} from '@mui/material'
+import { Navbar,  Feed,ChannelDetail,SearchFeed, VideoDetail }from './components';
+ 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+       <Box sx={{backgroundColor:'#000'}} >
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+       </Box>
+    </BrowserRouter>
   )
 }
 
---
+++ youtube_app/src/components/SearchFeed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchFeed = () => {
+  return (
+    <div>Searchfeed</div>
+  )
+}
+
+export default SearchFeed;
